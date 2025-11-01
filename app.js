ede2c80b55284a201ae13b58b81d6003bcc6f6d1
kpuhififmipaunpad-make <kpuhifi.fmipaunpad@gmail.com>
2025-11-01 11:44:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,66 +2,79 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');   // bisa diapus gak nih
-const flash  = require('connect-flash');
+const logger = require('morgan');
+const flash = require('connect-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
 const passport = require('passport');
 
-// passport config
+// Passport config
 require("./config/passport")(passport);
 
-// router
+// Router
 const usersRouter = require('./app/users/router');
 const dashboardRouter = require('./app/dashboard/router');
 const authRouter = require('./app/auth/router');
 
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Session & Flash
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
-  cookie: { maxAge:86400000 }
+  cookie: { 
+    maxAge: 86400000, // 24 hours
+    secure: false,
+    httpOnly: true
+  }
 }));
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'));
 
-app.use((req,res,next)=> {
+// Global variables for templates
+app.use((req, res, next) => {
   res.locals.error_msg = req.flash('error_msg');
-  res.locals.error  = req.flash('error');
-next();
-})
+  res.locals.error = req.flash('error');
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.user = req.user || null;
+  next();
+});
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/adminlte', express.static(path.join(__dirname, '/node_modules/admin-lte/')));
 
+// Routes
 app.use('/', usersRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/auth', authRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
